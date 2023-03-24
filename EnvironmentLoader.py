f25bcdb24ec5c9774186d50d7d3a50b85d7699ac
Can Karka <53074877+Cankar001@users.noreply.github.com>
2023-03-24 17:26:12 +0100

    Added EnvironmentLoader and updated Logger impl

---
+++ EnvironmentLoader.py
@@ -0,0 +1,43 @@
+import os
+
+import Logger
+
+def load():
+    """
+    This function parses a .env file and returns its content as a map.
+    """
+    if not os.path.exists('.env') and not os.path.exists('../.env'):
+        Logger.error('Could not find any .env in the current or parent folder!')
+        return dict()
+
+    result = dict()
+
+    # Load text content
+    env_text = ''
+    if os.path.exists('.env'):
+        with open('.env', 'r') as f:
+            env_text = f.readlines()
+
+    if len(env_text) == 0 and os.path.exists('../.env'):
+        with open('../.env', 'r') as f:
+            env_text = f.readlines()
+
+    if len(env_text) == 0:
+        Logger.error('Could not load env file!')
+        return dict()
+
+    for line in env_text:
+        if line == '\n':
+            continue
+
+        if line.endswith('\n'):
+            line = line.strip('\n')
+
+        key_value_pairs = line.split('=')
+        key = key_value_pairs[0]
+        value = key_value_pairs[1]
+
+        result[key] = value
+
+    Logger.success('Successfully loaded .env file')
+    return result
--- Logger.py
+++ Logger.py
@@ -1,24 +1,79 @@
 from colorama import Fore
 from colorama import Style
 
+LOGGER_ENABLED = True
+DEBUGGING_ENABLED = False
+
+def enableDebugger():
+    global DEBUGGING_ENABLED
+    DEBUGGING_ENABLED = True
+
+
+def enableLogger():
+    global LOGGER_ENABLED
+    LOGGER_ENABLED = True
+
+
+def disableLogger():
+    global LOGGER_ENABLED
+    LOGGER_ENABLED = False
+
+
 def debug(msg):
-    print(f"{Style.BRIGHT}{Fore.CYAN}" + str(msg) + f"{Style.RESET_ALL}")
+    global LOGGER_ENABLED
+    global DEBUGGING_ENABLED
+
+    if not LOGGER_ENABLED:
+        return
+
+    if DEBUGGING_ENABLED:
+        print(f"{Style.BRIGHT}{Fore.CYAN}" + str(msg) + f"{Style.RESET_ALL}")
 
 def trace(msg):
+    global LOGGER_ENABLED
+
+    if not LOGGER_ENABLED:
+        return
+
     print(f"{Style.BRIGHT}{Fore.WHITE}" + str(msg) + f"{Style.RESET_ALL}")
 
 def error(msg):
+    global LOGGER_ENABLED
+
+    if not LOGGER_ENABLED:
+        return
+
     print(f"{Style.BRIGHT}{Fore.RED}" + '[ERROR]: ' + str(msg) + f"{Style.RESET_ALL}")
 
 def critical(msg):
+    global LOGGER_ENABLED
+
+    if not LOGGER_ENABLED:
+        return
+
     print(f"{Style.BRIGHT}{Fore.RED}" + '[CRITICAL ERROR]: ' + str(msg) + f"{Style.RESET_ALL}")
 
 def info(msg):
+    global LOGGER_ENABLED
+
+    if not LOGGER_ENABLED:
+        return
+
     print(f"{Style.BRIGHT}{Fore.BLUE}" + str(msg) + f"{Style.RESET_ALL}")
 
 def warn(msg):
+    global LOGGER_ENABLED
+
+    if not LOGGER_ENABLED:
+        return
+
     print(f"{Style.BRIGHT}{Fore.YELLOW}" + '[WARNING]: ' + str(msg) + f"{Style.RESET_ALL}")
 
 def success(msg):
+    global LOGGER_ENABLED
+
+    if not LOGGER_ENABLED:
+        return
+
     print(f"{Style.BRIGHT}{Fore.GREEN}" + str(msg) + f"{Style.RESET_ALL}")
 
